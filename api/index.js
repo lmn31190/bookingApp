05a0968bfea04013faaa29f5bb6148ef409a89f4
Monie <monielouis31@mail.com>
2023-05-08 13:18:49 +0200

    Add Sass

--- api/index.js
+++ api/index.js
@@ -29,6 +29,16 @@ mongoose.connection.on("disconnected", () => {
 
 //Middleware
 
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if(req.method ==='OPTION'){
+      res.header("Access-Control-Allow-Methods", 'GET,POST,PUT,DELETE,PATCH');
+      return res.status(200).json({});
+  }
+  next();
+});
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
