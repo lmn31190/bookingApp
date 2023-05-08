432527d457fea854d873b5704378bdbe89b98b1c
Monie <monielouis31@mail.com>
2023-05-08 13:04:38 +0200

    Add Sass

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,7 @@
 import "./register.scss";
 import axios from "axios";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
 
 const Register = ({ inputs, title }) => {
   const [file, setFile] = useState("");
